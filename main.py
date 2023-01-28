ca25897f0b386d383cf45a5442e9c7ca4689e739
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-28 11:07:52 +0200

    request logic for sunrise-sunset API

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+
+
+MY_LATITUDE = 44.426765
+MY_LONGITUDE = 26.102537
+
+# Parameters needed for sunrise-sunset API request:
+location_parameters = {
+    "lat": MY_LATITUDE,
+    "lng": MY_LONGITUDE
+}
+
+# Make request and get response from API endpoint:
+response = requests.get(
+    "https://api.sunrise-sunset.org/json", params=location_parameters)
+
+# Raise exception for unsuccessful status code (anything other than 200)
+response.raise_for_status()
+data = response.json()
