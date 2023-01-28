702fb17434fdc743cd959a56e80c6b85e33d3b27
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-28 11:15:46 +0200

    added additional parameter to format response from API

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 
 MY_LATITUDE = 44.426765
@@ -7,7 +8,8 @@ MY_LONGITUDE = 26.102537
 # Parameters needed for sunrise-sunset API request:
 location_parameters = {
     "lat": MY_LATITUDE,
-    "lng": MY_LONGITUDE
+    "lng": MY_LONGITUDE,
+    "formatted": 0
 }
 
 # Make request and get response from API endpoint:
@@ -19,4 +21,3 @@ response.raise_for_status()
 data = response.json()
 sunrise = data["results"]["sunrise"]
 sunset = data["results"]["sunset"]
-print(sunrise, sunset)
